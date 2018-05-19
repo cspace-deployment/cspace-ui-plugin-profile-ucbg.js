5ee1320e8e9220d6d2d0c62030eaf28aba21ec86
Cesar Villalobos <cesarv.h@berkeley.edu>
2018-05-19 14:51:51 -0700

    UCBG-549: added columns.js, reasonForMove not showing in UI

--- src/plugins/recordTypes/movement/index.js
+++ src/plugins/recordTypes/movement/index.js
@@ -1,4 +1,5 @@
 import advancedSearch from './advancedSearch';
+import columns from './columns';
 import fields from './fields';
 import forms from './forms';
 
@@ -6,6 +7,7 @@ export default () => configContext => ({
   recordTypes: {
     movement: {
       advancedSearch: advancedSearch(configContext),
+      columns: columns(configContext),
       forms: forms(configContext),
       fields: fields(configContext),
     },
