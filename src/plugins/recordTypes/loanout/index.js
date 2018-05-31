a72ca8554b82c6398cbd7443c9eb64e2d5e2f378
Cesar Villalobos <cesarv.h@berkeley.edu>
2018-05-31 15:11:42 -0700

    Added columns.js

--- src/plugins/recordTypes/loanout/index.js
+++ src/plugins/recordTypes/loanout/index.js
@@ -1,8 +1,9 @@
 import advancedSearch from './advancedSearch';
+import columns from './columns';
 import fields from './fields';
+import forms from './forms';
 import idGenerators from './idGenerators';
 import title from './title';
-import forms from './forms';
 import optionLists from './optionLists';
 
 export default () => configContext => ({
@@ -11,6 +12,7 @@ export default () => configContext => ({
   recordTypes: {
     loanout: {
       advancedSearch: advancedSearch(configContext),
+      columns: columns(configContext),
       fields: fields(configContext),
       forms: forms(configContext),
       title: title(configContext),
