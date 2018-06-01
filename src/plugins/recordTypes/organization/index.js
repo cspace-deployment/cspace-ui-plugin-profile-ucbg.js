95feafbd87d34658c55ce4c3c1c09da5d19c5144
Cesar Villalobos <cesarv.h@berkeley.edu>
2018-05-31 17:08:43 -0700

    UCBG-451: Added advanced search

--- src/plugins/recordTypes/organization/index.js
+++ src/plugins/recordTypes/organization/index.js
@@ -1,3 +1,4 @@
+import advancedSearch from './advancedSearch';
 import fields from './fields';
 import forms from './forms';
 import vocabularies from './vocabularies';
@@ -5,6 +6,7 @@ import vocabularies from './vocabularies';
 export default () => configContext => ({
   recordTypes: {
     organization: {
+      advancedSearch: advancedSearch(configContext),
       fields: fields(configContext),
       forms: forms(configContext),
       vocabularies,
