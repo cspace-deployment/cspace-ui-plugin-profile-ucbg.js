3d7d29894acf78ad000afa753f01c08039fa0d69
Ray Lee <rhlee@berkeley.edu>
2018-07-10 01:06:57 -0700

    UCBG-444: Reload taxon records after update, so listener changes are reflected.

--- src/plugins/recordTypes/taxon/index.js
+++ src/plugins/recordTypes/taxon/index.js
@@ -14,6 +14,11 @@ export default () => configContext => ({
       fields: fields(configContext),
       forms: forms(configContext),
       columns: columns(configContext),
+      // If a child taxon is added or removed, an event listener may cause the access code to be
+      // updated. The payload returned by the services layer reflects the saved taxon before
+      // listeners triggered by added/removed hierarchy relations have run, so these changes to the
+      // access code do not appear. To work around this, refetch the taxon record after update.
+      refetchAfterUpdate: true,
     },
   },
 });
