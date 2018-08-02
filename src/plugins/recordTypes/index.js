9caded386edf2f36c116adf712b63616797d7eac
Ray Lee <rhlee@berkeley.edu>
2018-08-01 18:50:29 -0700

    UCBG-484: Hide loanin and acquisition.

--- src/plugins/recordTypes/index.js
+++ src/plugins/recordTypes/index.js
@@ -1,9 +1,11 @@
+import acquisition from './acquisition';
 import citation from './citation';
 import collectionobject from './collectionobject';
 import intake from './intake';
 import loanout from './loanout';
 import concept from './concept';
 import location from './location';
+import loanin from './loanin';
 import media from './media';
 import movement from './movement';
 import organization from './organization';
@@ -16,12 +18,14 @@ import taxon from './taxon';
 import work from './work';
 
 export default [
+  acquisition,
   citation,
   collectionobject,
   intake,
   loanout,
   concept,
   location,
+  loanin,
   media,
   movement,
   organization,
