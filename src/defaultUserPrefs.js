d608db3ffba6ebb6bf2ba0f615f8f78090c198b8
Cesar Villalobos <cesarv.h@berkeley.edu>
2018-08-09 15:33:59 -0700

    UCBG-487: Added all secondary tabs to all records, to be cleaned later

--- src/defaultUserPrefs.js
+++ src/defaultUserPrefs.js
@@ -192,4 +192,17 @@ export default {
       },
     },
   },
+  recordBrowserNavBarItems: {
+    collectionobject: ['collectionobject', 'conditioncheck', 'movement', 'objectexit', 'group', 'intake', 'media', 'pottag', 'propagation', 'valuation', 'loanout'],
+    conditioncheck: ['collectionobject', 'conditioncheck', 'movement', 'objectexit', 'group', 'intake', 'media', 'pottag', 'propagation', 'valuation', 'loanout'],
+    movement: ['collectionobject', 'conditioncheck', 'movement', 'objectexit', 'group', 'intake', 'media', 'pottag', 'propagation', 'valuation', 'loanout'],
+    objectexit: ['collectionobject', 'conditioncheck', 'movement', 'objectexit', 'group', 'intake', 'media', 'pottag', 'propagation', 'valuation', 'loanout'],
+    group: ['collectionobject', 'conditioncheck', 'movement', 'objectexit', 'group', 'intake', 'media', 'pottag', 'propagation', 'valuation', 'loanout'],
+    intake: ['collectionobject', 'conditioncheck', 'movement', 'objectexit', 'group', 'intake', 'media', 'pottag', 'propagation', 'valuation', 'loanout'],
+    media: ['collectionobject', 'conditioncheck', 'movement', 'objectexit', 'group', 'intake', 'media', 'pottag', 'propagation', 'valuation', 'loanout'],
+    pottag: ['collectionobject', 'conditioncheck', 'movement', 'objectexit', 'group', 'intake', 'media', 'pottag', 'propagation', 'valuation', 'loanout'],
+    propagation: ['collectionobject', 'conditioncheck', 'movement', 'objectexit', 'group', 'intake', 'media', 'pottag', 'propagation', 'valuation', 'loanout'],
+    valuation: ['collectionobject', 'conditioncheck', 'movement', 'objectexit', 'group', 'intake', 'media', 'pottag', 'propagation', 'valuation', 'loanout'],
+    loanout: ['collectionobject', 'conditioncheck', 'movement', 'objectexit', 'group', 'intake', 'media', 'pottag', 'propagation', 'valuation', 'loanout'],
+  },
 };
