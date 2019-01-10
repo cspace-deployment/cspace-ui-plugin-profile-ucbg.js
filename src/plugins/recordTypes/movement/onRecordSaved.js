4658fd38e5e9b8f8b2652d59d16d8d62d4d8f009
cesarvh <cesarv.h@berkeley.edu>
2019-01-09 17:10:01 -0800

    BG-17: onRecordSaved.js now checks for full action code URN

--- src/plugins/recordTypes/movement/onRecordSaved.js
+++ src/plugins/recordTypes/movement/onRecordSaved.js
@@ -1,8 +1,9 @@
 export default ({ data, recordEditor }) => {
   if (data) {
     const actionCode = data.getIn(['document', 'ns2:movements_common', 'reasonForMove']);
+    const deadActCodeUrn = "urn:cspace:botgarden.cspace.berkeley.edu:vocabularies:name(actionCode):item:name(actCode00)'Dead'";
 
-    if (actionCode && actionCode === 'Dead') {
+    if (actionCode && actionCode === deadActCodeUrn) {
       recordEditor.delete();
     }
   }
