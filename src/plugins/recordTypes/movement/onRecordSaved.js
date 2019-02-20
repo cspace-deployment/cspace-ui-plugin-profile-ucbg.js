68ce1993beeb4fb98403faafc9537e7c33ae6c5b
Cesar Villalobos-Huizar <cesarv.h@berkeley.edu>
2019-02-20 14:23:16 -0800

    BG-17: Changed comparison in case 'Dead' is changed

--- src/plugins/recordTypes/movement/onRecordSaved.js
+++ src/plugins/recordTypes/movement/onRecordSaved.js
@@ -1,9 +1,9 @@
 export default ({ data, recordEditor }) => {
   if (data) {
     const actionCode = data.getIn(['document', 'ns2:movements_common', 'reasonForMove']);
-    const deadActCodeUrn = "urn:cspace:botgarden.cspace.berkeley.edu:vocabularies:name(actionCode):item:name(actCode00)'Dead'";
+    const deadActCodeUrn = "urn:cspace:botgarden.cspace.berkeley.edu:vocabularies:name(actionCode):item:name(actCode00)";
 
-    if (actionCode && actionCode === deadActCodeUrn) {
+    if (actionCode && actionCode.includes(deadActCodeUrn)) {
       recordEditor.delete();
     }
   }
