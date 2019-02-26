85ba6aa0d56e6cd4b32dbdfffa69db0513c4e696
Cesar Villalobos-Huizar <cesarv.h@berkeley.edu>
2019-02-26 13:12:49 -0800

    BG-17: fixed travis build

--- src/plugins/recordTypes/movement/onRecordSaved.js
+++ src/plugins/recordTypes/movement/onRecordSaved.js
@@ -1,7 +1,7 @@
 export default ({ data, recordEditor }) => {
   if (data) {
     const actionCode = data.getIn(['document', 'ns2:movements_common', 'reasonForMove']);
-    const deadActCodeUrn = "urn:cspace:botgarden.cspace.berkeley.edu:vocabularies:name(actionCode):item:name(actCode00)";
+    const deadActCodeUrn = 'urn:cspace:botgarden.cspace.berkeley.edu:vocabularies:name(actionCode):item:name(actCode00)';
 
     if (actionCode && actionCode.includes(deadActCodeUrn)) {
       recordEditor.delete();
