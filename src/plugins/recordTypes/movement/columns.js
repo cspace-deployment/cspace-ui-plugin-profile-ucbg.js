53e8f66cbba207d61b53c6198aac0d5c5832f68d
cesarvh <cesarv.h@berkeley.edu>
2019-04-02 15:36:48 -0700

    CC-454: Changed formatting of ActionCode column in advanced search

--- src/plugins/recordTypes/movement/columns.js
+++ src/plugins/recordTypes/movement/columns.js
@@ -1,24 +1,31 @@
 import { defineMessages } from 'react-intl';
 
-export default () => ({
-  default: {
-    movementReferenceNumber: {
-      disabled: true,
+export default (configContext) => {
+  const {
+    formatRefName,
+  } = configContext.formatHelpers;
+
+  return {
+    default: {
+      movementReferenceNumber: {
+        disabled: true,
+      },
+      currentLocation: {
+        order: 20,
+        width: 250,
+      },
+      reasonForMove: {
+        formatValue: formatRefName,
+        messages: defineMessages({
+          label: {
+            id: 'column.movement.default.reasonForMove',
+            defaultMessage: 'Action code',
+          },
+        }),
+        order: 30,
+        sortBy: 'movements_common:reasonForMove',
+        width: 380,
+      },
     },
-    currentLocation: {
-      order: 20,
-      width: 250,
-    },
-    reasonForMove: {
-      messages: defineMessages({
-        label: {
-          id: 'column.movement.default.reasonForMove',
-          defaultMessage: 'Action code',
-        },
-      }),
-      order: 30,
-      sortBy: 'movements_common:reasonForMove',
-      width: 380,
-    },
-  },
-});
+  };
+};
