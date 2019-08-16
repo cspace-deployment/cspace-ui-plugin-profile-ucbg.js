710abc27bb3eec56753bab1262c4c0bc80c1b0eb
cesarvh <cesarv.h@berkeley.edu>
2019-08-16 15:00:59 -0400

    CC-728: Added parameters for UOCApprovalStatus

--- src/plugins/invocables/report/ucbgUOCApprovalStatus/fields.js
+++ src/plugins/invocables/report/ucbgUOCApprovalStatus/fields.js
@@ -4,6 +4,10 @@ export default (configContext) => {
   const {
     CompoundInput,
     OptionPickerInput,
+    TermPickerInput,
+    DateInput,
+    TextInput,
+    AutocompleteInput,
   } = configContext.inputComponents;
 
   const {
@@ -26,11 +30,88 @@ export default (configContext) => {
               defaultMessage: 'Output format',
             },
           }),
+          required: true,
           view: {
             type: OptionPickerInput,
             props: {
               source: 'uocApprovalStatusMIMETypes',
             },
+          },
+        },
+      },
+      Requester: {
+        [config]: {
+          messages: defineMessages({
+            name: {
+              id: 'field.report.botgardenUOCApprovalStatus.Requester.name',
+              defaultMessage: 'Requester',
+            },
+          }),
+          view: {
+            type: AutocompleteInput,
+            props: {
+              source: 'person/local,person/shared,person/ulan,organization/local,organization/shared,organization/collective,organization/ulan',
+              showQuickAdd: false,
+            }
+          },
+        }
+      },
+      AuthBy: {
+        [config]: {
+          messages: defineMessages({
+            name: {
+              id: 'field.report.botgardenUOCApprovalStatus.AuthBy.name',
+              defaultMessage: 'Authorized by',
+            },
+          }),
+          view: {
+            type: AutocompleteInput,
+            props: {
+              source: 'person/local,person/shared,person/ulan,organization/local,organization/shared,organization/collective,organization/ulan',
+              showQuickAdd: false,
+            },
+          },
+        },
+      },
+      AuthStatus: {
+        [config]: {
+          messages: defineMessages({
+            name: {
+              id: 'field.report.botgardenUOCApprovalStatus.AuthStatus.name',
+              defaultMessage: 'Authorization status',
+            },
+          }),
+          view: {
+            type: TermPickerInput,
+            props: {
+              source: 'uocauthorizationstatuses',
+            },
+          },
+        },
+      },
+      StartDate: {
+        [config]: {
+          messages: defineMessages({
+            name: {
+              id: 'field.report.botgardenUOCApprovalStatus.StartDate.name',
+              defaultMessage: 'Earliest requested date',
+            },
+          }),
+          view: {
+            type: DateInput,
+          },
+        },
+      },
+      EndDate: {
+        [config]: {
+          messages: defineMessages({
+            name: {
+              id: 'field.report.botgardenUOCApprovalStatus.EndDate.name',
+              defaultMessage: 'Latest requested date',
+            },
+          }),
+          view: {
+            type: DateInput,
           },
         },
       },
