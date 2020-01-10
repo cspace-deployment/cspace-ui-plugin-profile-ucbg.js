c935e31a5af9b41ca3f380ba1dc143ea27510e13
cesarvh <cesarv.h@berkeley.edu>
2020-01-10 11:45:06 -0800

    CC-979: Removing deads in bed report config as it is no longer needed

--- src/plugins/invocables/report/index.js
+++ src/plugins/invocables/report/index.js
@@ -1,5 +1,4 @@
 import ucbgDeadReportRange from './ucbgDeadReportRange';
-import ucbgDiedInLocation from './ucbgDiedInLocation';
 import ucbgRareStatusFamily from './ucbgRareStatusFamily';
 import ucbgRareStatusGenus from './ucbgRareStatusGenus';
 import ucbgVoucherFamily from './ucbgVoucherFamily';
@@ -7,15 +6,9 @@ import ucbgVoucherGenus from './ucbgVoucherGenus';
 import ucbgUOCApprovalStatus from './ucbgUOCApprovalStatus';
 import ucbgUOCbyRequesterObject from './ucbgUOCbyRequesterObject';
 import ucbgAccessionsByBedDate from './ucbgAccessionsByBedDate';
-import ucbgDiedInLocationExcel from './ucbgDiedInLocationExcel';
-// import ucbgAccessionCount from './ucbgAccessionCount';
-// import ucbgListofLivingAccessions from './ucbgListofLivingAccessions';
-// import ucbgTaxonCount from './ucbgTaxonCount';
-// import ucbgVoucherLabel from './ucbgVoucherLabel';
 
 export default [
   ucbgDeadReportRange,
-  ucbgDiedInLocation,
   ucbgRareStatusFamily,
   ucbgRareStatusGenus,
   ucbgVoucherFamily,
@@ -23,9 +16,4 @@ export default [
   ucbgUOCApprovalStatus,
   ucbgUOCbyRequesterObject,
   ucbgAccessionsByBedDate,
-  ucbgDiedInLocationExcel,
-  // ucbgAccessionCount, // No parameters, no doctype. Works if put it in the collectionobjects tab
-  // ucbgListofLivingAccessions, // same as above
-  // ucbgTaxonCount, // same as above
-  // ucbgVoucherLabel, // same as above
 ];
