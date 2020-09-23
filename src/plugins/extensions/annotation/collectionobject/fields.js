b2c0aa3d7a97a60e5c8169f1823b8d47516c5796
cesarvh <cesarv.h@berkeley.edu>
2020-09-23 15:00:47 -0700

    CC-1312: Added config for annotation extension

---
+++ src/plugins/extensions/annotation/collectionobject/fields.js
@@ -0,0 +1,30 @@
+import { defineMessages } from 'react-intl';
+
+export default (configContext) => {
+  const {
+    OptionPickerInput,
+  } = configContext.inputComponents;
+
+  const {
+    configKey: config,
+  } = configContext.configHelpers;
+
+  return {
+    'ns2:collectionobjects_annotation': {
+      annotationGroupList: {
+        annotationGroup: {
+          annotationType: {
+            [config]: {
+              view: {
+                type: OptionPickerInput,
+                props: {
+                  source: 'annotationTypes',
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  };
+};
