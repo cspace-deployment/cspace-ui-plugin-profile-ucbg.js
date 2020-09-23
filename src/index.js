8f13c2eaa8c3f73ab7ef1208cc1886a01cb5abe9
Richard Millet <remillet@gmail.com>
2020-09-22 14:04:31 -1000

    CC-1310: Misc lint fixes and imports.

--- src/index.js
+++ src/index.js
@@ -42,6 +42,7 @@ module.exports = () => ({
   },
   plugins: [
     cspaceUIPluginExtLocality(),
+    accessionUseExtensionPlugin(),
     cspaceUIPluginRecordTaxon(),
     cspaceUIPluginExtUCBNHCollectionObject(),
     cspaceUIPluginExtUCBNHConcept(),
