49be78940ad7ae14b526bb0de942e451e4763580
Lam Voong <lkv@berkeley.edu>
2021-04-14 07:58:06 -0700

    CC-1458: update fields.js to remove required

--- src/plugins/invocables/report/ucbgRareStatusFamily/fields.js
+++ src/plugins/invocables/report/ucbgRareStatusFamily/fields.js
@@ -25,7 +25,6 @@ export default (configContext) => {
               defaultMessage: 'Family',
             },
           }),
-          required: true,
           view: {
             type: AutocompleteInput,
             props: {
